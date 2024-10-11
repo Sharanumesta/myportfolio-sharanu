f5329d58b90fbb2b78647278a618473cb4f47f19
Shara <115623797+Sharanumesta@users.noreply.github.com>
2024-10-11 13:55:02 +0000

    commit 2.3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/myportfolio-sharanu/',
+  base: '/myportfolio-sharanu',
   plugins: [react()],
 });
